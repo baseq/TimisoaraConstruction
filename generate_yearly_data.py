6b88fe72dfbf20b29fccd5862801d8d93de417cb
deathy <deathy@deathy.ro>
2015-11-10 23:02:19 +0200

    Removed inaccurate/approximate locations. Added markers.

--- generate_yearly_data.py
+++ generate_yearly_data.py
@@ -9,10 +9,21 @@ records_by_year = {}
 for record in all_records:
     year = record['Data'][record['Data'].rfind('.') + 1:]
     if not year in records_by_year.keys():
-        records_by_year[year] = []
+        records_by_year[year] = {}
+        records_by_year[year]['points'] = []
+        records_by_year[year]['accurate'] = 0
+        records_by_year[year]['ignored'] = 0
     if len(record['geocode']['results']) > 0:
-        if record['geocode']['results'][0]['geometry']['location']['lat'] != 45.7488716:
-            records_by_year[year].append(record['geocode']['results'][0]['geometry']['location'])
+        if record['geocode']['results'][0]['geometry']['location_type'] == 'ROOFTOP':
+            new_item = record['geocode']['results'][0]['geometry']['location']
+            new_item['data'] = record['Data']
+            new_item['adresa'] = record['Adresa']
+            new_item['beneficiar'] = record['Beneficiar']
+            new_item['descriere'] = record['Descriere']
+            records_by_year[year]['points'].append(record['geocode']['results'][0]['geometry']['location'])
+            records_by_year[year]['accurate'] += 1
+        else:
+            records_by_year[year]['ignored'] += 1
 
 for year in records_by_year.keys():
     with open('site/yearly_data/' + year + '.json', 'w') as outfile:
